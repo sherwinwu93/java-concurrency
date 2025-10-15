19ea6a7db22a90ae19d565329681e029358effd9
wusd <nanxihope@163.com>
2025-10-15 17:48:31 +0800

    --

---
+++ src/c03/PriorityTest.java
@@ -0,0 +1,16 @@
+package c03;
+
+/**
+ * @author Wusd
+ * @date 2025/10/15
+ * @description
+ */
+public class PriorityTest {
+    public static void main(String[] args) {
+        Thread a = new Thread();
+        System.out.println("默认优先级:" + a.getPriority());
+        Thread b = new Thread();
+        b.setPriority(Thread.MAX_PRIORITY);
+        System.out.println("设置过的优先级:" + b.getPriority());
+    }
+}
